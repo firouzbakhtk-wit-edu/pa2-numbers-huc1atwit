18f648d39428ee052ab6b3286908e00ae038dc16
chris <chris@10.220.18.142>
2024-10-15 09:14:30 -0400

    LA2

---
+++ src/edu/wit/cs/comp1000/LA1.java
@@ -0,0 +1,36 @@
+package edu.wit.cs.comp1000;
+
+import java.util.Scanner;
+
+public class LA1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		double num;
+		double sum = 0;
+		double avg = 0;
+		double sd = 0;
+		double[] sdarray = new double[5];
+		
+		System.out.printf("Enter five numbers :");
+		for (int i = 0; i < 5; i++) {
+			num = input.nextDouble();
+			sdarray[i]= num;
+			sum += num;
+			avg = sum/5;
+		}
+		
+		int length = sdarray.length;
+	    for (double number : sdarray) {
+	        sd += (number - avg) * (number - avg);
+	    }
+	    sd = Math.sqrt(sd / length);
+	    
+		System.out.println("Sum: " + sum);
+		System.out.println("Mean: " + avg);
+		System.out.printf("population Standard Deviation: %.2f", sd);
+
+	}
+
+}
