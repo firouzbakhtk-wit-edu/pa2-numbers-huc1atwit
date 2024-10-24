7d61d24d0d237bf3faa14e474e1088e5ed5c51be
Chris Hu <huc1@wit.edu>
2024-10-24 16:16:50 -0400

    PA2 Final code

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,68 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+/**
+ * Solution to PA2a.
+ * Takes in 5 numbers and calculates sum and averages of all, positive, and non-positive numbers
+ * 
+ * @author Chris Hu
+ */
 public class PA2a {
-
-	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+		
+		Scanner input = new Scanner(System.in);
+		int sumpos = 0;
+		double avgpos = 0;
+		
+		int sumneg = 0;
+		double avgneg = 0;
+		
+		int poscount = 0;
+		int negcount = 0;
+		
+		int sumall = 0;
+		double avgall = 0;
+		
+		System.out.printf("Enter five whole numbers: ");
+		
+		for (int i = 0; i < 5; i++) {
+			double num = input.nextDouble();
+			if (num>0) {
+				poscount++;
+				sumpos += num;
+				avgpos = ((double)sumpos/poscount);
+			} else {
+				negcount++;
+				sumneg += num;
+				avgneg = ((double)sumneg/negcount);
+			}
+			sumall+=num;
+			
+		}
+		avgall = (double)sumall/5;
+		if (poscount == 1) {
+			System.out.println("The sum of the " + poscount + " positive number: " + sumpos);
+		} else {
+			System.out.println("The sum of the " + poscount + " positive numbers: " + sumpos);
+		}
+		if (negcount == 1) {
+			System.out.println("The sum of the " + negcount + " non-positive number: " + sumneg);
+		} else {
+			System.out.println("The sum of the " + negcount + " non-positive numbers: " + sumneg);
+		}
+		System.out.println("The sum of the 5 numbers: " + sumall);
+		
+		if (poscount == 1) {
+			System.out.printf("The average of the " + poscount + " positive number: %.2f%n", avgpos);
+		} else {
+			System.out.printf("The average of the " + poscount + " positive numbers: %.2f%n", avgpos);
+		}
+		if (negcount == 1) {
+			System.out.printf("The average of the " + negcount + " non-positive number: %.2f%n", avgneg);
+		} else {
+			System.out.printf("The average of the " + negcount + " non-positive numbers: %.2f%n", avgneg);
+		}
 
+		System.out.printf("The average of the 5 numbers: %.2f%n", avgall);
+	}
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,46 +1,42 @@
 package edu.wit.cs.comp1000;
 
 import java.util.Scanner;
-// TODO: document this class
+/**
+ * Solution to PA2b.
+ * Calculates the quadratic formula
+ * 
+ * @author Chris Hu
+ */
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
 		Scanner input = new Scanner(System.in);
-		double a;
-		double b;
-		double c;
-		double d;
-		double roota;
-		double rootb;
-		
 		System.out.print("Enter a b c: ");
 		for (int i = 0; i < 3 ; i++) {
-			a = input.nextDouble();
-			b = input.nextDouble();
-			c = input.nextDouble();
-			d = b * b - 4 * a * c;
+			double a = input.nextDouble();
+			double b = input.nextDouble();
+			double c = input.nextDouble();
+			double d = Math.pow(b, 2) - 4 * a * c;
 			
 			if (d < 0) {
 				System.out.println("Roots: imaginary");
 				break;
 			} else if (d == 0){
 				d = Math.sqrt(d);
-				roota = (b * -1 + d) / 2;
-				System.out.println("Root: " + roota);
+				double roota = (b * -1 + d) / 2;
+				System.out.printf("Root: %.2f%n", roota);
 				break;
 			}
 				else {
 			
 				d = Math.sqrt(d);
-				rootb = (b * -1 - d) / 2;
-				roota = (b * -1 + d) / 2;
-				System.out.println("Roots: " + roota + ", " + rootb);
+				double rootb = (b * -1 - d) / (2 * a);
+				double roota = (b * -1 + d) / (2 * a);
+				System.out.printf("Roots: %.2f, %.2f%n", rootb , roota);
 				break;
 			}
 		}
-		
 	}
-
 }
